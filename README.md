c9353edd2e385b340eb3329a2ff8c1a92b23d874
John Hodges <jdhmtl@users.noreply.github.com>
2021-04-02 14:13:11 -0400

    Merged in configs from work

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dotfiles
+
+A collection of configuration files for commonly-used applications. At present, the expected destination for this repository is `~/.dotfiles`, though that can easily be a symlink.
+
+## Local configuration
+
+To allow for location specific configurations, files ending in `.local` will be ignored by Git.
---
+++ config/fzf.zsh
@@ -0,0 +1,22 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="$PATH:/usr/local/opt/fzf/bin"
+fi
+
+export FZF_DEFAULT_OPTS='--layout=reverse --height=20 --border'
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# FZF + Ag
+if [ -e /usr/local/bin/ag ]; then
+  export FZF_DEFAULT_COMMAND='ag --hidden -g "" -U --ignore .git'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
